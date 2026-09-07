8623ce0a84b7c9b22700fd074aae02ff678b5687
Stefan Bergli <ascendens@hotmail.com>
2026-09-07 19:19:03 +0200

    Uppgift 2 for

--- src/main/java/Exersice.java
+++ src/main/java/Exersice.java
@@ -2,9 +2,8 @@ public class Exersice {
 
     static public void main(){
 
-        for (int variabel = 65536; variabel > 2; ) {
+        for (int variabel = 65536; variabel > 2; variabel = variabel >> 1) {
             IO.println(variabel);
-            variabel = variabel >> 1;
 
         }
 
