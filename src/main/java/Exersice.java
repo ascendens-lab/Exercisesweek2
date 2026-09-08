da5a2d2742d5648606898fbdfa3f0ad23681d86f
Stefan Bergli <ascendens@hotmail.com>
2026-09-08 08:44:57 +0200

    Uppgift 6 nästlad

--- src/main/java/Exersice.java
+++ src/main/java/Exersice.java
@@ -4,33 +4,18 @@ public class Exersice {
 
     static public void main(){
 
-      long number = Math.round(Math.random() * 99 + 1);
-      int guesses = 0;
+        for(int y=0; y<(5); y++) {
+            for(int x=0; x<5; x++) {
+                if( x == y )
+                    System.out.print("#");
+                else
+                    System.out.print(".");
+            }
+            System.out.println("");
+        }
 
-      boolean b = true ;
-
-      while (b) {
-          int guess = Integer.parseInt(IO.readln("Gissa ett på ett tal 1-100: "));
-          guesses++;
-
-          if (guess < number)
-              IO.println("Talet är större.");
-
-          else if (guess > number)
-              IO.println("Talet är mindre.");
-
-          else {
-              IO.println("Rätt gissat, talet var " + number + "!");
-              IO.println("Du behövde gissningar" + guesses + " för att gissa rätt.");
-              b = false;
-      }
+    }
       }
 
 
-    }
-
-
-
-
-    }
 
